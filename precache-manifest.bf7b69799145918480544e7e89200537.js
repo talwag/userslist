f6a07625a171ff18158df348d00500d255797da5
talwag <wagtal@gmail.com>
2020-05-27 19:31:23 +0300

    Updates

---
+++ precache-manifest.bf7b69799145918480544e7e89200537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff3ccc4687e91fe14282dbb4173a71f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfd591743729b218b4d0",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3cd8fad59eaf7e3b32f6",
+    "url": "/static/css/main.05deeb5f.chunk.css"
+  },
+  {
+    "revision": "bfd591743729b218b4d0",
+    "url": "/static/js/2.9af384db.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.9af384db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cd8fad59eaf7e3b32f6",
+    "url": "/static/js/main.e5c6bcba.chunk.js"
+  },
+  {
+    "revision": "90a99b63518dfa43c607",
+    "url": "/static/js/runtime-main.6e6a6807.js"
+  }
+]);
